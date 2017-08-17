f0f769adfe54df3c0669c1f8fa3a5549f757b682
raymond.lee <raymond.lee@nsuslab.com>
2017-08-17 10:38:05 +0900

    *추가한 파일입니다.

---
+++ sample/Class_process/InfoProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using sample.Models;
+
+namespace sample.Class_process
+{
+    public class InfoProcess
+    {
+        public String SetNowDate(PersonalInfo info)
+        {
+            info.NowDate = DateTime.Now.ToShortDateString();
+
+            JavaScriptSerializer json_par = new JavaScriptSerializer();
+            String dataJson    = json_par.Serialize(info);
+            return dataJson;
+        }
+    }
+}
---
+++ sample/Controllers/PersonalInfoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using sample.Models;
+using sample.Class_process;
+
+namespace sample.Controllers
+{
+    public class PersonalInfoController : Controller
+    {
+        // GET: PersonalInfo
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public string GetBonusCheck(string bonusCode)
+        {
+            var data = "?BonusCode=" + bonusCode;
+            return data;
+        }
+
+        [HttpPost]
+        public String TestDTO(PersonalInfo info)
+        {
+            InfoProcess info_process = new InfoProcess();
+            string obj  = info_process.SetNowDate(info);
+
+            return obj;
+            
+        }
+    }
+
+}
